fe85f7786a6e7bedc2c7bd9afdeb44bcc82bb894
MrMartinMystery <martinmystery369@gmail.com>
2025-12-24 02:39:09 -0500

    Enhance SaavnSearch with audio playback and recommendations
    Refactor SaavnSearch functionality to include audio playback and song recommendation features.

--- assets/js/saavn-search.js
+++ assets/js/saavn-search.js
@@ -1,141 +1,122 @@
-var results_container = document.querySelector("#saavn-results")
-var results_objects = {};
-const searchUrl = "https://jiosaavn-api-privatecvc2.vercel.app/search/songs?query=";
-function SaavnSearch() {
-event.preventDefault(); // stop page changing to #, which will reload the page
-var query = document.querySelector("#saavn-search-box").value.trim()
-query = encodeURIComponent(query);
+// --- COMPLETE UPDATED functions.js ---
 
-if(query==lastSearch) {doSaavnSearch(query)}
-    window.location.hash = lastSearch; 
-if(query.length > 0) { 
-    window.location.hash = query 
-}
+// Track user history in local storage to remember "past choices"
+let playHistory = JSON.parse(localStorage.getItem('saavn_history')) || {};
 
-}
-var page_index = 1;
-function nextPage() {
-    var query = document.querySelector("#saavn-search-box").value.trim();
-    if (!query) {query = lastSearch;}
-    query = encodeURIComponent(query);
-    doSaavnSearch(query,0,true)
-}
-async function doSaavnSearch(query,NotScroll,page) {
-    window.location.hash = query;
-    document.querySelector("#saavn-search-box").value = decodeURIComponent(query);
-    if(!query) {return 0;}
-results_container.innerHTML = `<span class="loader">Searching</span>`;
-    query=query+"&limit=40";
-    if(page) {
-        ;page_index=page_index+1;query=query+"&page="+page_index;
-    } else {query=query+"&page=1";page_index=1;}
-    
-// try catch
-try {
-var response = await fetch(searchUrl + query);
-} catch(error) {
-results_container.innerHTML = `<span class="error">Error: ${error} <br> Check if API is down </span>`;
-}
-var json = await response.json();
-/* If response code isn't 200, display error*/
-if (response.status !== 200) {
-    results_container.innerHTML = `<span class="error">Error: ${json.message}</span>`;
-    console.log(response)
-    return 0;
-}
-var json = json.data.results;
-var results = [];
-if(!json) {results_container.innerHTML = "<p> No result found. Try other Library </p>";return;}
-lastSearch = decodeURI(window.location.hash.substring(1));
-for(let track of json) {
+function PlayAudio(audio_url, song_id) {
+    var audio = document.getElementById('player');
+    var source = document.getElementById('audioSource');
+    source.src = audio_url;
 
+    // Get track details from the UI
+    var name = document.getElementById(song_id + "-n").textContent;
+    var album = document.getElementById(song_id + "-a").textContent;
+    var image = document.getElementById(song_id + "-i").getAttribute("src");
 
-song_name = TextAbstract(track.name,25);
-album_name = TextAbstract(track.album.name,20);
-if (track.album.name == track.name) {
-    album_name = ""
-}
-var measuredTime = new Date(null);
-measuredTime.setSeconds(track.duration); // specify value of SECONDS
-var play_time = measuredTime.toISOString().substr(11, 8);
-if (play_time.startsWith("00:0")) {
-    play_time = play_time.slice(4);
-}
-if (play_time.startsWith("00:")) {
-    play_time = play_time.slice(3);
-}
-var song_id = track.id;
-var year = track.year;
-var song_image = track.image[1].link;
-var song_artist = TextAbstract(track.primaryArtists,30);
-var bitrate = document.getElementById('saavn-bitrate');
-var bitrate_i = bitrate.options[bitrate.selectedIndex].value;
-if(track.downloadUrl) {
-var download_url = track.downloadUrl[bitrate_i]['link'];
-var quality = "";
-if (bitrate_i == 4) {quality = 320} else {quality = 160;}
-    // push object to results array
-    results_objects[song_id] = {
-        track: track
+    // Update Player UI
+    document.title = name + " - " + album;
+    document.getElementById("player-name").innerHTML = name;
+    document.getElementById("player-album").innerHTML = album;
+    document.getElementById("player-image").setAttribute("src", image);
+
+    // Save choice to history
+    playHistory[song_id] = (playHistory[song_id] || 0) + 1;
+    localStorage.setItem('saavn_history', JSON.stringify(playHistory));
+
+    audio.load();
+    audio.play();
+
+    // AUTO-PLAY: When the current song ends, fetch the next recommendation
+    audio.onended = function() {
+        console.log("Song ended. Playing next based on your choice...");
+        playNextRecommendation(song_id);
     };
-      results.push(`
-      <div class="text-left song-container" style="margin-bottom:20px;border-radius:10px;background-color:#1c1c1c;padding:10px;">
-      <div class="row" style="margin:auto;">
-          <div class="col-auto" style="padding:0px;padding-right:0px;border-style:none;">
-              <img id="${song_id}-i" class="img-fluid d-inline" style="width:115px;border-radius:5px;height:115px;padding-right:10px;" src="${song_image}" loading="lazy"/>
-          </div>
-          <div class="col" style="border-style:none;padding:2px;">
-              <p class="float-right fit-content" style="margin:0px;color:#fff;padding-right:10px;">${year}</p>
-              <p id="${song_id}-n" class="fit-content" style="margin:0px;color:#fff;max-width:100%;">${song_name}</p>
-              <p id="${song_id}-a" class="fit-content" style="margin:0px;color:#fff;max-width:100%;">${album_name}<br/></p>
-              <p id="${song_id}-ar" class="fit-content" style="margin:0px;color:#fff;max-width:100%;">${song_artist}<br/></p>
-              <button class="btn btn-primary song-btn" type="button" style="margin:0px 2px;" onclick='PlayAudio("${download_url}","${song_id}")'>▶</button>
-              <button class="btn btn-primary song-btn" type="button" style="margin:0px 2px;" onclick='AddDownload("${song_id}")'>DL</button>
-              <p class="float-right fit-content" style="margin:0px;color:#fff;padding-right:10px;padding-top:15px;">${play_time}<br/></p>
-          </div>
-      </div>
-  </div>
-`
-); }
-    }
-    
-    results_container.innerHTML = results.join(' ');
-    if(!NotScroll){
-    document.getElementById("saavn-results").scrollIntoView();
-    }
-
-
 }
-function TextAbstract(text, length) {
-    if (text == null) {
-        return "";
+
+async function playNextRecommendation(currentSongId) {
+    [cite_start]// API endpoint for song suggestions [cite: 4]
+    const recUrl = `https://jiosaavn-api-privatecvc2.vercel.app/songs/${currentSongId}/suggestions`;
+    try {
+        const response = await fetch(recUrl);
+        const json = await response.json();
+        
+        if (json.data && json.data.length > 0) {
+            // Pick the first recommended track
+            const nextTrack = json.data[0];
+            
+            // Determine bitrate
+            var bitrate = document.getElementById('saavn-bitrate');
+            var bitrate_i = bitrate.options[bitrate.selectedIndex].value;
+            const nextUrl = nextTrack.downloadUrl[bitrate_i]?.link || nextTrack.downloadUrl[3].link;
+            
+            // Map data so PlayAudio can find it
+            results_objects[nextTrack.id] = { track: nextTrack };
+            
+            // Inject virtual elements so the UI doesn't break
+            const vDom = document.createElement('div');
+            vDom.style.display = 'none';
+            vDom.innerHTML = `
+                <span id="${nextTrack.id}-n">${nextTrack.name}</span>
+                <span id="${nextTrack.id}-a">${nextTrack.album.name}</span>
+                <img id="${nextTrack.id}-i" src="${nextTrack.image[2].link}">
+            `;
+            document.body.appendChild(vDom);
+
+            // Play the recommended song
+            PlayAudio(nextUrl, nextTrack.id);
+        }
+    } catch (error) {
+        console.error("Auto-play error:", error);
     }
-    if (text.length <= length) {
-        return text;
-    }
-    text = text.substring(0, length);
-    last = text.lastIndexOf(" ");
-    text = text.substring(0, last);
-    return text + "...";
 }
-if(window.location.hash) {
-   doSaavnSearch(window.location.hash.substring(1));
-} else {doSaavnSearch('english',1);}
 
-addEventListener('hashchange', event => { });
-onhashchange = event => {doSaavnSearch(window.location.hash.substring(1))};
+function searchSong(search_term) {
+    document.getElementById('saavn-search-box').value = search_term;
+    var goButton = document.getElementById("saavn-search-trigger");
+    goButton.click();
+}
 
-// If Bitrate changes, search again
-$('#saavn-bitrate').on('change', function () {
-    doSaavnSearch(lastSearch);
-        /*
-    var isDirty = !this.options[this.selectedIndex].defaultSelected;
-
-    if (isDirty) {
-        // Value Changed
-        doSaavnSearch(lastSearch)
-    } else {
-        // Do Nothing
-    } */
-});
-document.getElementById("loadmore").addEventListener('click',nextPage)
+// Download Logic
+var DOWNLOAD_API = "https://openmp3compiler.astudy.org"
+function AddDownload(id) {
+    var MP3DL = DOWNLOAD_API + "/add?id=" + id;
+    fetch(MP3DL)
+    .then(response => response.json())
+    .then(data => {
+        if (data.status == "success") {
+            var download_list = document.getElementById("download-list");
+            var download_item = document.createElement("li");
+            download_item.innerHTML = `
+                <div class="col">
+                    <img class="track-img" src="${data.image}" width="50px">
+                    <div style="display: inline;">
+                        <span class="track-name">${id}</span> - 
+                        <span class="track-album">${data.album}</span>
+                        <br>
+                        <span class="track-size">Size: Null</span>
+                        <span class="track-status" style="color:green"></span>
+                    </div>
+                </div><hr>`;
+            download_item.setAttribute("track_tag", id);
+            download_item.className = "no-bullets";
+            download_list.appendChild(download_item);
+            
+            var download_status_span = document.querySelector('[track_tag="'+id+'"] .track-status');
+            download_status_span.innerHTML = data.status;
+            
+            var interval = setInterval(function() {
+                fetch(DOWNLOAD_API + "/status?id=" + id)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status) {
+                        download_status_span.textContent = data.status;
+                        if (data.status == "Done") {
+                            download_status_span.innerHTML = `<a href="${DOWNLOAD_API}${data.url}" target="_blank">Download MP3</a>`;
+                            clearInterval(interval);
+                        }
+                    }
+                });
+            }, 3000);
+        } 
+    });
+}
